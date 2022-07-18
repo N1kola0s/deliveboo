64998ed9fcc4b241d45d0bac993187a50c928292
Dev1 <robertodelprete1993@libero.it>
2022-07-18 15:54:19 +0200

    Fix validation create close #47

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -12,25 +12,37 @@
     <!-- Input del Nome -->
     <div class="mb-4">
         <label for="name">Nome *</label>
-        <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror" placeholder="Scrivi nome piatto" aria-describedby="nameHelper" value="{{old('name')}}" required>
-        <small id="nameHelper" class="text-muted">Max 150 Caratteri</small>
+        <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror" placeholder="Scrivi nome piatto" aria-describedby="nameHelper" value="{{old('name')}}" maxlength="50" minlength="3" pattern="[A-Z a-z]{0,50}" required>
+        <small id="nameHelper" class="text-muted">Max 50 Caratteri</small>
+        @error('cover_img')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ 'Solo lettere' }}</strong>
+        </span>
+        @enderror
     </div>
     <!-- Input immagine del prodotto -->
     <div class="mb-4">
         <label for="cover_img">Immagine *</label>
         <input type="file" name="cover_img" id="cover_img" class="form-control @error('cover_img') is-invalid @enderror" placeholder="Learn php article" aria-describedby="cover_imgHelper" required>
-        <small id="cover_img" class="text-muted">Immagine del Prodotto</small>
+        <small id="cover_img" class="text-muted">Max 500kb</small>
+
+        @error('cover_img')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ 'Immagine peso massimo 500kb formati accettati jpeg,png,svg,jpg' }}</strong>
+        </span>
+        @enderror
+
     </div>
-     <!-- Input prezzo del prodotto -->
+    <!-- Input prezzo del prodotto -->
     <div class="mb-4">
         <label for="price">Prezzo *</label>
         <input type="number" step="0.01" name="price" id="price" class="form-control @error('price') is-invalid @enderror" placeholder="Scrivi il prezzo" aria-describedby="priceHelper" value="{{old('price')}}" required>
         <small id="priceHelper" class="text-muted">Solo Numeri</small>
-    </div>
-    <!-- Toggle del visibility -->
-    <div class="form-check form-switch mb-4">
-        <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked  name='visibility'>
-        <label class="form-check-label" for="flexSwitchCheckChecked">Seleziona visibilità</label>
+        @error('cover_img')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ 'Min 0.01-Max 99.99' }}</strong>
+        </span>
+        @enderror
     </div>
     <!-- Contenuto del post -->
     <div class="mb-4">
