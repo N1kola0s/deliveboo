ca01390a2d0ddb2cf1b6a3650d3e11cf1030caac
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-07-22 17:04:35 +0200

    add home layouts

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,25 +1,64 @@
 <template>
-  
+
   <div class="home_page">
-      <JumboSection />
+    <JumboSection />
 
+    <!-- RISTORANTI che poi vanno filtrati e TIPI  -->   
+    <div class="container py-5">
+       <div class="types py-3">
+      <a  class="mx-2 my-1 btn btn-primary" v-for="type in types" :key="type.id">
+        {{type.name}}
+      </a>
+    </div>
 
-      <div class="container py-5">
-        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4">
+      <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4">
 
-          <div class="col" v-for="restaurant in restaurants" :key="restaurant.id">
-          
-            <div class="restaurant">
-              <img class="img-fluid" :src="restaurant.cover_img" :alt="restaurant.business_name">
-            </div>
-           
+        <div class="col" v-for="restaurant in restaurantsResponse.data" :key="restaurant.id">
+
+          <div class="restaurant">
+            <img class="img-fluid"
+              :src="restaurant.cover_img.includes('uploads') ? '/storage/' + restaurant.cover_img : restaurant.cover_img"
+              :alt="restaurant.business_name">
           </div>
 
         </div>
+
       </div>
-      <WorkWithUs />
-      <AboutUs />
-      <FooterSection />
+
+      <!-- PAGINAZIONE -->
+      <div class="py-2 justify-content-center">
+        <nav aria-label="Page navigation" class="py-5">
+          <ul class="pagination justify-content-center">
+            <li class="page-item" v-if="restaurantsResponse.current_page > 1">
+              <a class="page-link" aria-label="Previous"
+                @click.prevent="getRestaurants(restaurantsResponse.current_page - 1)">
+                <span aria-hidden="true">&laquo;</span>
+                <span class="visually-hidden">Previous</span>
+              </a>
+            </li>
+
+            <li :class="{ 'page-item': true, 'active': page == restaurantsResponse.current_page }"
+              v-for="page in restaurantsResponse.last_page" :key="page.index">
+
+              <a class="page-link" @click.prevent="getRestaurants(page)">
+                {{ page }}
+              </a>
+            </li>
+
+            <li class="page-item" v-if="restaurantsResponse.current_page < restaurantsResponse.last_page">
+              <a class="page-link"  aria-label="Next"
+                @click.prevent="getRestaurants(restaurantsResponse.current_page + 1)">
+                <span aria-hidden="true">&raquo;</span>
+                <span class="visually-hidden">Next</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+    <WorkWithUs />
+    <AboutUs />
+    <FooterSection />
 
   </div>
 
@@ -43,18 +82,42 @@ export default {
   },
   data() {
     return {
-      restaurants: '',
+      /* restaurants: '', */ // senza paginazione
+      restaurantsResponse: '', // per paginazione
+      types: '',
+    }
+  },
+  methods: {
+    getRestaurants(restaurantPage) {
+      axios
+        .get('/api/restaurants', {
+          params: {
+            page: restaurantPage,
+          }
+        })
+        .then((response) => {
+          /* console.log(response); */
+          /* this.restaurants = response.data.data */ // senza paginazione
+          this.restaurantsResponse = response.data // con paginazione
+        }).catch(e => {
+          console.error(e);
+        })
+    },
+    getTypes() {
+      axios
+        .get('/api/types')
+        .then((response) => {
+          console.log(response);
+          this.types = response.data 
+
+        }).catch(e => {
+          console.error(e);
+        })
     }
   },
   mounted() {
-    axios
-    .get('/api/')
-      .then((response) => {
-        /* console.log(response); */
-        this.restaurants = response.data.data
-      }).catch(e => {
-        console.error(e);
-      })
+    this.getRestaurants(1);
+    this.getTypes();
   }
 }
 </script>
