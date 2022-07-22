58fe6c7cbf5246f48fc3c61f4093cd287ba900b3
aciernoantonio <aciernoantonio.int@gmail.com>
2022-07-22 17:01:44 +0200

    sezione lavora con noi close #104

--- resources/js/components/WorkWithUs.vue
+++ resources/js/components/WorkWithUs.vue
@@ -1,6 +1,18 @@
 <template>
-    <div class="container py-5">
-        LAVORA CON NOI
+    <div class="py-5">
+        <div class="p-5 bg-dark">
+            <div class="container text-white">
+                <h1 class="display-3 pb-3">Lavora con noi</h1>
+                <p class="lead w-50 pb-4">
+                    Diventa nostro partner e raggiungi più clienti con Deliveboo. Mentre noi ci occupiamo
+                    della consegna tu puoi offrire i migliori piatti per i tuoi clienti.
+                    </p>
+
+                <p class="lead">
+                    <a class="btn btn-danger text-white btn-lg" href="Jumbo action link" role="button">Scopri di più</a>
+                </p>
+            </div>
+        </div>
     </div>
 </template>
 
