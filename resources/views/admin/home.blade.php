628f8f3e9e59ce0288d667c03802482796bee31a
aciernoantonio <aciernoantonio.int@gmail.com>
2022-07-25 15:51:07 +0200

    modifica view admin.blade close #66

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,28 +5,28 @@
 <div class="container">
 
     {{-- resturant card --}}
-    <div class="main-card rounded-3 shadow">
-        <div class="row justify-content-center">
-            <!-- Immagine dell'account utente -->
-            <div class="col-7">
-                @if(str_contains($user[0]->cover_img, '/img/'))
-                    <img class="img-fluid resturant-img rounded-start" src="{{$user[0]->cover_img}}" alt="{{$user[0]->name}}">
-                @else
-                    <img class="img-fluid resturant-img rounded-start" src="{{asset('storage/' . $user[0]->cover_img)}}" alt="{{$user[0]->name}}">
-                @endif
-            </div>
 
-            <div class="col-5">
-                <div class="card-body">
-                    <h3 class="card-title">{{$user[0]->business_name}}</h3>
-                    <address class="text-muted pb-3">{{$user[0]->city}}, {{$user[0]->address}}, {{$user[0]->zip_code}}</address>
+    <div class="main-card row justify-content-center rounded-3 shadow">
+        <!-- Immagine dell'account utente -->
+        <div class="col-xs-12 col-md-7 px-0">
+            @if(str_contains($user[0]->cover_img, '/img/'))
+                <img class="img-fluid resturant-img rounded-3" src="{{$user[0]->cover_img}}" alt="{{$user[0]->name}}">
+            @else
+                <img class="img-fluid resturant-img rounded-3" src="{{asset('storage/' . $user[0]->cover_img)}}" alt="{{$user[0]->name}}">
+            @endif
+        </div>
 
-                    <h5 class="pb-2">{{$user[0]->telephone_number}}</h5>
-                    <h5>{{$user[0]->email}}</h5>
-                </div>
+        <div class="col-md-5">
+            <div class="card-body">
+                <h3 class="card-title">{{$user[0]->business_name}}</h3>
+                <address class="text-muted pb-3">{{$user[0]->city}}, {{$user[0]->address}}, {{$user[0]->zip_code}}</address>
+
+                <h5 class="pb-2">{{$user[0]->telephone_number}}</h5>
+                <h5>{{$user[0]->email}}</h5>
             </div>
         </div>
     </div>
+
 
     <div class="row my-4">
 
@@ -34,15 +34,15 @@
         <div class="col me-3 rounded-3 shadow">
             <div class="row justify-content-center">
                 <!-- Intestazione utente -->
-                <div class="card-header bg-dark text-white">
+                <div class="card-header text-white">
                     Utente
                 </div>
                 <!-- Immagine profilo -->
-                <div class="col-3">
-                    immagine avatar
+                <div class="col-md-3 py-3">
+                    <img class="img-fluid rounded-circle" src="{{asset('img/avatar_5.jpg')}}" alt="">
                 </div>
                 <!-- Nome e cognome profilo -->
-                <div class="col-9">
+                <div class="col-md-9">
                     <div class="card-body">
                         <h3 class="card-title">{{$user[0]->name}} {{$user[0]->surname}}</h3>
                     </div>
@@ -50,9 +50,9 @@
             </div>
         </div>
 
-        <div class="col me-3 p-0 rounded-3 shadow">
+        <div class="col p-0 rounded-3 shadow analythics">
             <!-- Intestazione della colonna -->
-            <div class="card-header bg-dark text-white">
+            <div class="card-header text-white">
                 Analythics
             </div>
             <!-- Grafico dei prodotti -->
