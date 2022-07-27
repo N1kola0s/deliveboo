7969e8432893b117f22dd4bc13864b3e5b77245a
aciernoantonio <aciernoantonio.int@gmail.com>
2022-07-27 17:50:01 +0200

    Layout dei prodotti parte Main close #147

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -6,96 +6,111 @@
     <!-- RISTORANTI che poi vanno filtrati e TIPI  -->
     <div class="container py-5">
 
-      <!-- TYPES -->
-      <div class="label-div d-flex">
+      <div class="row justify-content-center">
 
-        <div class="p-2" v-for="(type, index) in types" :key="index">
-          <label class="p-1" :class="{ checked: checkedTypes.includes(type.id) }" :for="type.id">{{ type.name }}
-            <input type="checkbox" :name="type" :id="type.id" :value="type.id" v-model="checkedTypes"
-              @change="searching()">
-          </label>
+        <div class="col-2">
+
+            <div class="py-3">Categories</div>
+
+          <!-- TYPES -->
+          <div class="label-div">
+            
+
+            <div class="p-2" v-for="(type, index) in types" :key="index">
+              <label class="p-1 form-check-label position-relative ps-4" :class="{ checked: checkedTypes.includes(type.id) }" :for="type.id">{{ type.name }}
+                <input type="checkbox" class="form-check-label position-absolute start-0 mt-1" :name="type" :id="type.id" :value="type.id" v-model="checkedTypes"
+                  @change="searching()">
+              </label>
+            </div>
+          </div>
+
         </div>
-      </div>
 
-      <!-- RISTORANTI FILTRATI -->
-      <div v-if="checkedTypes.length != 0" class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4">
-        <div class="col" v-for="restaurant in filteredRestaurants" :key="restaurant.id">
-          <router-link :to="{ name: 'restaurant', params: { 'slug': restaurant.slug } }" class="restaurant">
-            <img class="img-fluid"
-              :src="restaurant.cover_img.includes('uploads') ? '/storage/' + restaurant.cover_img : restaurant.cover_img"
-              :alt="restaurant.business_name">
-          </router-link>
+        <div class="col-10">
+
+          <!-- RISTORANTI FILTRATI -->
+          <div v-if="checkedTypes.length != 0" class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4">
+            <div class="col food_card rounded-3" v-for="restaurant in filteredRestaurants" :key="restaurant.id">
+              <router-link :to="{ name: 'restaurant', params: { 'slug': restaurant.slug } }" class="restaurant">
+                <img class="img-fluid rounded-3"
+                  :src="restaurant.cover_img.includes('uploads') ? '/storage/' + restaurant.cover_img : restaurant.cover_img"
+                  :alt="restaurant.business_name">
+              </router-link>
+            </div>
+
+          </div>
+          <!-- RISTORANTI PRIMA CHIAMATA -->
+          <div v-else class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4">
+            <div class="col food_card rounded-3" v-for="restaurant in restaurantsResponse.data" :key="restaurant.id">
+              <router-link :to="{ name: 'restaurant', params: { 'slug': restaurant.slug } }" class="restaurant">
+                <img class="img-fluid rounded-3"
+                  :src="restaurant.cover_img.includes('uploads') ? '/storage/' + restaurant.cover_img : restaurant.cover_img"
+                  :alt="restaurant.business_name">
+              </router-link>
+            </div>
+          </div>
+
+          <!-- PAGINAZIONE -->
+          <div v-if="checkedTypes.length != 0" class="py-2 justify-content-center">
+            <nav aria-label="Page navigation" class="py-5">
+              <ul class="pagination justify-content-center">
+                <li class="page-item" v-if="pagination.current > 1">
+                  <a class="page-link" aria-label="Previous" @click.prevent="searching(pagination.current - 1)">
+                    <span aria-hidden="true">&laquo;</span>
+                    <span class="visually-hidden">Previous</span>
+                  </a>
+                </li>
+
+                <li :class="{ 'page-item': true, 'active': page == pagination.current }"
+                  v-for="(page, index) in pagination.maxPages" :key="index">
+
+                  <a class="page-link" @click.prevent="getRestaurants(page)">
+                    {{ page }}
+                  </a>
+                </li>
+
+                <li class="page-item" v-if="pagination.current < pagination.maxPages">
+                  <a class="page-link" aria-label="Next" @click.prevent="searching(pagination.current + 1)">
+                    <span aria-hidden="true">&raquo;</span>
+                    <span class="visually-hidden">Next</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <div v-else class="py-2 justify-content-center">
+            <nav aria-label="Page navigation" class="py-5">
+              <ul class="pagination justify-content-center">
+                <li class="page-item" v-if="restaurantsResponse.current_page > 1">
+                  <a class="page-link" aria-label="Previous"
+                    @click.prevent="getRestaurants(restaurantsResponse.current_page - 1)">
+                    <span aria-hidden="true">&laquo;</span>
+                    <span class="visually-hidden">Previous</span>
+                  </a>
+                </li>
+
+                <li :class="{ 'page-item': true, 'active': page == restaurantsResponse.current_page }"
+                  v-for="page in restaurantsResponse.last_page" :key="page.index">
+
+                  <a class="page-link" @click.prevent="getRestaurants(page)">
+                    {{ page }}
+                  </a>
+                </li>
+
+                <li class="page-item" v-if="restaurantsResponse.current_page < restaurantsResponse.last_page">
+                  <a class="page-link" aria-label="Next"
+                    @click.prevent="getRestaurants(restaurantsResponse.current_page + 1)">
+                    <span aria-hidden="true">&raquo;</span>
+                    <span class="visually-hidden">Next</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
         </div>
 
-      </div>
-      <!-- RISTORANTI PRIMA CHIAMATA -->
-      <div v-else class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4">
-        <div class="col" v-for="restaurant in restaurantsResponse.data" :key="restaurant.id">
-          <router-link :to="{ name: 'restaurant', params: { 'slug': restaurant.slug } }" class="restaurant">
-            <img class="img-fluid"
-              :src="restaurant.cover_img.includes('uploads') ? '/storage/' + restaurant.cover_img : restaurant.cover_img"
-              :alt="restaurant.business_name">
-          </router-link>
-        </div>
-      </div>
-
-      <!-- PAGINAZIONE -->
-      <div v-if="checkedTypes.length != 0" class="py-2 justify-content-center">
-        <nav aria-label="Page navigation" class="py-5">
-          <ul class="pagination justify-content-center">
-            <li class="page-item" v-if="pagination.current > 1">
-              <a class="page-link" aria-label="Previous" @click.prevent="searching(pagination.current - 1)">
-                <span aria-hidden="true">&laquo;</span>
-                <span class="visually-hidden">Previous</span>
-              </a>
-            </li>
-
-            <li :class="{ 'page-item': true, 'active': page == pagination.current }"
-              v-for="(page, index) in pagination.maxPages" :key="index">
-
-              <a class="page-link" @click.prevent="getRestaurants(page)">
-                {{ page }}
-              </a>
-            </li>
-
-            <li class="page-item" v-if="pagination.current < pagination.maxPages">
-              <a class="page-link" aria-label="Next" @click.prevent="searching(pagination.current + 1)">
-                <span aria-hidden="true">&raquo;</span>
-                <span class="visually-hidden">Next</span>
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-
-      <div v-else class="py-2 justify-content-center">
-        <nav aria-label="Page navigation" class="py-5">
-          <ul class="pagination justify-content-center">
-            <li class="page-item" v-if="restaurantsResponse.current_page > 1">
-              <a class="page-link" aria-label="Previous"
-                @click.prevent="getRestaurants(restaurantsResponse.current_page - 1)">
-                <span aria-hidden="true">&laquo;</span>
-                <span class="visually-hidden">Previous</span>
-              </a>
-            </li>
-
-            <li :class="{ 'page-item': true, 'active': page == restaurantsResponse.current_page }"
-              v-for="page in restaurantsResponse.last_page" :key="page.index">
-
-              <a class="page-link" @click.prevent="getRestaurants(page)">
-                {{ page }}
-              </a>
-            </li>
-
-            <li class="page-item" v-if="restaurantsResponse.current_page < restaurantsResponse.last_page">
-              <a class="page-link" aria-label="Next"
-                @click.prevent="getRestaurants(restaurantsResponse.current_page + 1)">
-                <span aria-hidden="true">&raquo;</span>
-                <span class="visually-hidden">Next</span>
-              </a>
-            </li>
-          </ul>
-        </nav>
       </div>
 
     </div>
@@ -193,3 +208,17 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+.food_card{
+  height: 200px;
+}
+
+.food_card img{
+  height: 100%;
+  width: 100%;
+}
+
+</style>
+
