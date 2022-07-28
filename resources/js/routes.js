d7f2250a17e403f517d6105a089cc0ce5d0cf4eb
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-07-28 22:10:13 +0200

    implementazione modifiche router close #171

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,12 +1,18 @@
 import Home from "./Pages/Home";
 import Restaurant from "./Pages/Restaurant";
 import Checkout from "./Pages/Checkout";
+/* Nuovi Import */
+import ThankYou from "./Pages/ThankYou";
+import NotFound from "./pages/NotFound";
+
 
 const routes = [
     { path: '/', name: 'home', component: Home },
     { path: '/:slug', name: 'restaurant', component: Restaurant },
-    { path: '/checkout/:slug', name: 'checkout', component: Checkout }
-
+    { path: '/checkout/:slug', name: 'checkout', component: Checkout },
+    /* Nuovi componenti importati */
+    { path: '/checkout/:slug/thankyou', name: 'thankyou', component: ThankYou },
+    { path: "*", component: NotFound }
 ]
 
 
