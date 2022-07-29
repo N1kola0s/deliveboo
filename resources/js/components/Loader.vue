617e162f3359663507aa3c3d44ecc4a7fe49aba8
Dev1 <robertodelprete1993@libero.it>
2022-07-29 10:27:19 +0200

    Aggiunto redirect thankyou e loader close #176

---
+++ resources/js/components/Loader.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="loader" class="d-flex flex-column">
+        <img id="img-loader" src="/img/giphy-unscreen.gif" alt="ciao"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Loader'
+    }
+</script>
+
+<style scoped>
+#loader {
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	background-color: rgba(0, 0, 0, 0.5);
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	z-index: 100;
+}
+#img-loader {
+	width: 300px;
+	height: 300px;
+}
+</style>
