1188680b45ed06e724b61f5647398b5e66a0a9b0
Dev1 <robertodelprete1993@libero.it>
2022-07-31 15:39:47 +0200

    Aggiungere footer in checkout e color close #199

--- resources/js/Pages/Checkout.vue
+++ resources/js/Pages/Checkout.vue
@@ -3,7 +3,7 @@
     <div v-if="loadingGif">
       <Loader></Loader>
     </div>
-    <div v-if="loader">CARICAMENTO</div>
+    <div v-if="loader"><Loader></Loader></div>
     <div v-else>
       <div class="container">
         <h1 class="p-3">
@@ -257,6 +257,7 @@
         </div>
       </div>
     </div>
+    <footerSection></footerSection>
   </div>
 </template>
 
@@ -264,10 +265,12 @@
 <script>
 import axios from "axios";
 import Loader from "../components/Loader.vue";
+import FooterSection from "../components/FooterSection.vue";
 export default {
   name: "Checkout",
   components: {
     Loader,
+    FooterSection
   },
 
   data() {
@@ -418,4 +421,5 @@ export default {
     margin-right: 8px;
   }
 }
+
 </style>
